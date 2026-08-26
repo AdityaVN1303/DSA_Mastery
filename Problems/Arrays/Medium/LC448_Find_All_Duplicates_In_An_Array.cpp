47b0d2130e3c577fc4b3a6b15af61c178120a04b
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-27 02:09:49 +0530

    LC 448 Added

---
+++ Problems/Arrays/Medium/LC448_Find_All_Duplicates_In_An_Array.cpp
@@ -0,0 +1,23 @@
+// LC 448 - Find All Duplicates in an Array 
+
+// Approaches 
+
+// Sorting 
+// TC - O(NlogN)
+// SC - O(1)
+
+// Counting Sort 
+// TC - O(N + K)
+// SC - O(K)
+
+// Cyclic Sort 
+// TC - O(N)
+// SC - O(1)
+
+// Inplace Negation 
+// TC - O(N)
+// SC - O(1)
+
+// Hashing 
+// TC - O(N) , worst O(N^2)
+// SC - O(N)
