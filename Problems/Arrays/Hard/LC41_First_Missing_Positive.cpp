2a9c8a24096cd115792994d6b1bd783632f4ff0e
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-27 02:30:12 +0530

    lc 41 added and array hard folder created

---
+++ Problems/Arrays/Hard/LC41_First_Missing_Positive.cpp
@@ -0,0 +1,10 @@
+// LC 41 - First Missing Positive 
+
+// Approaches
+
+// All follows 1 Trick - Missing Positive will be within 1 and n+1
+
+// Sorting 
+// Inplace Negation (after cleaning)
+// Cyclic Sort 
+// Hashing 
