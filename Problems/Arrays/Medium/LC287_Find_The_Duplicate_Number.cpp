6e824b9f7d106cb8cd9f6c60dce344bc9b9c3dc9
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-27 01:28:34 +0530

    lc 287 added

---
+++ Problems/Arrays/Medium/LC287_Find_The_Duplicate_Number.cpp
@@ -0,0 +1,9 @@
+// LC 287 - Find Duplicate Number
+
+// Hashing Approach
+// Counting Sort 
+// Cyclic Sort 
+// Inplace Negation
+// Sorting 
+// Floyd Cycle Detection (Fast & Slow Pointers) (Optimal)
+// Binary Search on Answer (Optimal)
