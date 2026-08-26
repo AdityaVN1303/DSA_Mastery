38bed51cc799e7a547196854a093d28536932c56
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-26 21:08:17 +0530

    lc 448 added

---
+++ Problems/Arrays/Easy/LC448_Find_All_Numbers_Disappeared_In_An_Array.cpp
@@ -0,0 +1,19 @@
+// LC 448 - Find All Numbers Disappeared in an Array 
+
+// Approaches
+
+// Sorting 
+// TC - O(NlogN)
+// SC - O(1)
+
+// Counting Sort 
+// TC - O(N)
+// SC - O(N)
+
+// Hashset / Hashmap 
+// TC - O(N) , worst O(N^2)
+// SC - O(N)
+
+// Cyclic Sort (Optimal)
+// TC- O(N)
+// SC - O(1)
