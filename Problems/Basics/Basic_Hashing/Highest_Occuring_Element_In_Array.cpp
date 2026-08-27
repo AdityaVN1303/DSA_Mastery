8e533b97b5395441bee6a3a2d445686f122b7995
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-27 19:58:34 +0530

    Cleaned

--- Problems/Basics/Basic_Hashing/Highest_Occuring_Element_In_Array.cpp
+++ Problems/Basics/Basic_Hashing/Highest_Occuring_Element_In_Array.cpp
@@ -1,27 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Highest Occuring Element in an Array
-// TC - O(n)
-// SC - O(n)
-
-// Approaches --->
-
-// 1. Hashmap (OPTIMAL)
-// Optimal Aproach
-// O(N) TC
-// O(N) SC
-
-// 2. Sorting
-// O(NlogN) TC
-// O(1) SC
-
-// 3. Nested Loops (Brute)
-// O(N^2) TC
-// O(1) SC
-
-// Can be Solved with Hash Array based on Range of Input Elements
-
 int highestOccurringElement(const vector<int> &nums)
 {
     unordered_map<int, int> freqMap;
