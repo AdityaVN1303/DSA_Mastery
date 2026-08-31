62ebaf28df8dca98e45bbce852c487759d70f57f
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 18:55:55 +0530

    lc 128 code added

--- Problems/Arrays/Medium/LC128_Longest_Consecutive_Sequence.cpp
+++ Problems/Arrays/Medium/LC128_Longest_Consecutive_Sequence.cpp
@@ -1,15 +1,29 @@
 // LC 128 - Longest Consecutive Sequence
 
-// Approaches
-
-// Sorting
-// TC - O(NlogN)
-// SC - O(1)
+// Can be solved with Sorting or hashing or nested loops , but PS will not allow to run in more than O(N) TC.
 
 // Set (Optimal) Approach
-// TC - O(N) , worst O(N^2) 
+// TC - O(N) 
 // SC - O(N)
+// Failed at --- Iterating over nums instead of stt , causing TLE. (nums also contains duplicate elements)
+int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        unordered_set<int> stt(nums.begin() , nums.end());
 
-// Ordered Set 
-// TC - O(NlogN)
-// SC - O(N)
+        int count = 1;
+        int maxCount = 1;
+        for(int x : stt){
+            if(stt.find(x-1) == stt.end()){
+                int i=1;
+                while(stt.find(x+i) != stt.end()){
+                    count++;
+                    i++;
+                }
+                maxCount = max(maxCount , count);
+                count = 1;
+                i = 1;
+            }
+
+        }
+        return maxCount;
+    }
