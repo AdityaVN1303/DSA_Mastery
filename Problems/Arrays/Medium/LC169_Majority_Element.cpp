189064e56942a8ea73408d39e9f0953c24e0d3b7
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 19:29:59 +0530

    code for lc 169

--- Problems/Arrays/Medium/LC169_Majority_Element.cpp
+++ Problems/Arrays/Medium/LC169_Majority_Element.cpp
@@ -1,15 +1,22 @@
 // LC 169 - Majority Element 
 
-// Approaches
-
-// Hashmap 
-// TC - O(N)
-// SC - o(N)
-
-// Sorting
-// TC - O(NlogN)
-// SC - O(1)
+// Can also be solved with hashing and sorting but non optimal
 
 // Boyer-Moore Voting Algorithm 
 // TC - O(N)
 // SC - O(1)
+int majorityElement(vector<int>& nums) {
+        int count = 1;
+        
+        int num = nums[0];
+        for(int i=1; i<nums.size(); ++i){
+            if(nums[i] == num) count++;
+            else count--;
+
+            if(count <= 0){
+                count = 1;
+                num = nums[i];
+            }
+        }
+        return num;
+    }
