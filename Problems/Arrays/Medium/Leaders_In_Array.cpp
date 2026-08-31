d19d9ec9ace1b772a7cb358545fb85f6d942cf0b
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 22:42:35 +0530

    array leaders coded

--- Problems/Arrays/Medium/Leaders_In_Array.cpp
+++ Problems/Arrays/Medium/Leaders_In_Array.cpp
@@ -1,11 +1,16 @@
 // Leaders in An Array 
 
-// Approaches 
-
-// Nested Loops 
-// TC - O(N^2)
-// SC - O(1)
-
 // Linear Scan (Reverse) + Maintain Max from Right
 // TC - O(N)
 // SC - O(1)
+vector<int> leaders(vector<int>& arr) {
+        vector<int> ans;
+        int rightMax = 0;
+        for(int i=arr.size()-1; i>=0; --i){
+            if(arr[i] >= rightMax) ans.push_back(arr[i]);
+            
+            rightMax = max(arr[i] , rightMax);
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;
+    }
