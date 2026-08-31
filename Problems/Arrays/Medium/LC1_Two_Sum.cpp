090f6518bdc07024c8bc40041728102bcbd4627d
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 19:50:35 +0530

    lc 1 code added

--- Problems/Arrays/Medium/LC1_Two_Sum.cpp
+++ Problems/Arrays/Medium/LC1_Two_Sum.cpp
@@ -1,15 +1,26 @@
 // LC 1 - Two Sum 
 
-// Approaches 
+// Can also be solved with sorting
 
-// Prefix Sum + Hashmap
+//  Hashing (map)
 // TC - O(N) , worst O(N^2)
 // SC - O(N)
+vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int , int> mpp;
 
-// Sorting + Two Pointers (Opposite Ends)
-// Using Pairs for Storing Values with Indices 
-// TC - O(NlogN)
-// SC - O(N)
+        for(int i=0; i<nums.size(); ++i){
+            int compliment = target - nums[i];
+
+            if(mpp.find(compliment) != mpp.end()){
+                return vector<int>{mpp[compliment] , i};
+            }
+
+            mpp[nums[i]] = i;
+        }
+
+        return vector<int>{-1 , -1};
+    }
+
 
 // Nested Loops (Brute Force)
 // TC - O(N^2)
