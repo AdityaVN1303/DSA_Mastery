70a02555dca291a10170b9a6a8384a2f76c7a24c
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 18:36:27 +0530

    lc 121 code added

--- Problems/Arrays/Medium/LC121_Best_Time_To_Buy_And_Sell_Stocks.cpp
+++ Problems/Arrays/Medium/LC121_Best_Time_To_Buy_And_Sell_Stocks.cpp
@@ -1,17 +1,31 @@
 // LC 121 - Best Time to Buy and Sell Stock
 
-// Approaches
-
-// Nested Loops (Brute Force)
-// TC - O(N^2)
-// SC - O(1)
-
-// One Pass (Running Minimum)
-// You need best curretnVal - previous Minimum
+// Linear Pass (Maintain Lowest Buy Price)
 // TC - O(N)
 // SC - O(1)
+int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i=1; i<prices.size();++i){
+            maxProfit = max(maxProfit , prices[i]-minPrice);
+            
+            minPrice = min(minPrice , prices[i]);
+        }
+        return maxProfit;
+    }
 
-// Prefix Minimum Array
-// Build A prefix array with minimum val till i
-// TC - o(N)
-// SC - O(N)
+// Sliding Window (Variable)
+// TC - O(N)
+// SC - O(1)
+int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int maxProfit = 0;
+        for(int r=1; r<prices.size(); ++r){
+            maxProfit = max(maxProfit , prices[r] - prices[l]);
+
+            if(prices[r] < prices[l]){
+                l = r;
+            }
+        }
+        return maxProfit;
+    }
