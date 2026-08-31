c82a5f38ae3a272d59985ef0bb22f621dc5ee902
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 19:22:59 +0530

    LC 167 Code

--- Problems/Arrays/Medium/LC167_Two_Sum_II.cpp
+++ Problems/Arrays/Medium/LC167_Two_Sum_II.cpp
@@ -1,15 +1,22 @@
 // LC 167 - Two Sun II
 
-// Approaches
-
 // Two Pointers (Opposite Ends)
 // TC - O(N)
 // SC - O(1)
+vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0; 
+        int j = numbers.size()-1;
 
-// Binary Search (Searching for Compliment)
-// TC - O(NlogN)
-// SC - O(1)
-
-// Hashmap
-// TC - O(N) , Worst O(N^2)
-// SC - O(N)
+        while(i < j){
+            if(numbers[i] + numbers[j] == target){
+                return vector<int>{i+1 , j+1};
+            }
+            else if(numbers[i] + numbers[j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return vector<int>{-1 , -1};
+    }
