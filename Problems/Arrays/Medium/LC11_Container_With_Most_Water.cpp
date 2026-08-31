2faeb232e17847d1608310dad530b18cb437b889
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 17:53:06 +0530

    code written for lc 11

--- Problems/Arrays/Medium/LC11_Container_With_Most_Water.cpp
+++ Problems/Arrays/Medium/LC11_Container_With_Most_Water.cpp
@@ -2,13 +2,29 @@
 
 // Approaches 
 
+// Can also be solved with nested loops in O(N^2) TC
+
 // Two Pointers (Opposite Ends) (Optimal)
 // Optimization Done - Skip Heights Less than min(l , r)
-// Hack - Choosing Minimum of Both Ends
+// Observation - Choosing Minimum of Both Ends
+// Which Pointer to Move Inward ? Move the One with Smaller height. 
 // TC - O(N)
 // SC - O(1)
-
-// Nested Loop (Brute Force)
-// Finding All Pairs
-// TC - O(N^2)
-// SC - O(1)
+int maxArea(vector<int>& height) {
+        int i = 0;
+        int j = height.size()-1;
+        int water = 0;
+        int maxWater = 0;
+        while(i < j){
+            water = min(height[i] , height[j]) * (j-i);
+            maxWater = max(maxWater , water);
+            
+            if(height[i] > height[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return maxWater;
+    }
