8bf86775a0d74b7770fba5d88dae0e2fa51d0703
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 23:06:55 +0530

    lc 53 added

--- Problems/Arrays/Medium/LC53_Maximum_Subarray.cpp
+++ Problems/Arrays/Medium/LC53_Maximum_Subarray.cpp
@@ -1,15 +1,31 @@
 // LC 53 - Maximum Subarray 
 
-// Approaches
-
 // Kadane's Algorithm 
 // TC - O(N)
 // SC - O(1)
+int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int best = nums[0];
+        for(int i=0; i<nums.size(); ++i){
+            sum = max(nums[i] , sum+nums[i]);
+            best = max(best , sum);
+        }
+        return best;
+    }
 
 // Prefix Sum 
-// TC - O(N^2)
-// SC - O(N)
-
-// Nested Loops (Brute Force)
-// TC - O(N^3)
+// TC - O(N)
 // SC - O(1)
+// Observation => maxSum = currentSubarraySum - minimumSubarraySum
+int maxSubArray(vector<int>& nums) {
+        int currentSum = 0;
+        int maxSum = nums[0];
+        int minSum = 0;
+        for(int i=0; i<nums.size(); ++i){
+            currentSum += nums[i];
+            maxSum = max(maxSum , currentSum - minSum);
+            minSum = min(minSum , currentSum);
+        }
+        return maxSum;
+    }
+};
