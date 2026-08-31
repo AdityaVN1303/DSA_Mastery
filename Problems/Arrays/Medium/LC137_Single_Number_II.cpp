6af4d0526f9f24c0829a124de768752f944dfa26
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-08-31 19:19:30 +0530

    LC 137 Code Added

--- Problems/Arrays/Medium/LC137_Single_Number_II.cpp
+++ Problems/Arrays/Medium/LC137_Single_Number_II.cpp
@@ -1,11 +1,23 @@
 // LC 137 - Single Number II
 
-// Approaches 
+// Can be Solved with Bit Manipulation for Optimal Solution. 
 
-// Math - Multiplicity Difference (Hashset Optimized Approach)
+// Math - Multiplicity Difference (Hashset Optimized Approach) (Non Optimal)
 // TC - O(N)
 // SC - O(N)
+// Formula ---> 3*sum(unique) - sum(array) = 2*singleNumber
+int singleNumber(vector<int>& nums) {
+        unordered_set<int> stt(nums.begin() , nums.end());
+        
+        long long currentSum = 0;
+        for(int x : nums){
+            currentSum += x;
+        }
 
-// Hashmap (Frequency Count)
-// TC - O(N) 
-// SC - O(N)
+        long long totalSum = 0;
+        for(int x : stt){
+            totalSum += 3LL * x;
+        }
+
+        return (int) ((totalSum - currentSum)/2);
+    }
