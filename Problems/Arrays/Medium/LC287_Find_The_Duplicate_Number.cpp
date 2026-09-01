f678360e538b68398753d6267a0e6b3e4a74ed03
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-09-01 13:40:28 +0530

    lc 287 coded

--- Problems/Arrays/Medium/LC287_Find_The_Duplicate_Number.cpp
+++ Problems/Arrays/Medium/LC287_Find_The_Duplicate_Number.cpp
@@ -1,9 +1,39 @@
 // LC 287 - Find Duplicate Number
 
-// Hashing Approach
-// Counting Sort 
-// Cyclic Sort 
-// Inplace Negation
-// Sorting 
-// Floyd Cycle Detection (Fast & Slow Pointers) (Optimal)
-// Binary Search on Answer (Optimal)
+// Can also be solved with sorting and hashing (Non Optimal)
+
+// Question asks to solve in O(N) and in SC O(1) but without modfying the original array 
+
+
+// Cyclic Sort (Non Optimal)
+// TC - O(N)
+// SC - O(1)
+int findDuplicate(vector<int>& nums) {
+        int i=0; 
+        while(i < nums.size()){
+            int correct = nums[i] - 1;
+            if(nums[i] < nums.size() && nums[i] != nums[correct]){
+                swap(nums[i] , nums[correct]);
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] != i+1) return nums[i];
+        }
+        return -1;
+    }
+
+// Inplace Negation (Non Optimal)
+// TC - O(N)
+// SC - O(1)
+int findDuplicate(vector<int>& nums) {
+        for(int i=0; i<nums.size(); ++i){
+            int correctIdx = abs(nums[i])-1;
+            if(nums[correctIdx] > 0) nums[correctIdx] = -nums[correctIdx];
+            else return abs(nums[i]);
+        }
+        return -1;
+    }
