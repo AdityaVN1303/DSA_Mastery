8709fce15211645595d5174e5c427d914464bc3b
Aditya Nagare <117348400+AdityaVN1303@users.noreply.github.com>
2026-09-01 13:46:59 +0530

    lc 448 coded
    Implemented two methods to find all duplicates in an array: Cyclic Sort and Inplace Negation.

--- Problems/Arrays/Medium/LC448_Find_All_Duplicates_In_An_Array.cpp
+++ Problems/Arrays/Medium/LC448_Find_All_Duplicates_In_An_Array.cpp
@@ -1,23 +1,48 @@
 // LC 448 - Find All Duplicates in an Array 
 
-// Approaches 
-
-// Sorting 
-// TC - O(NlogN)
-// SC - O(1)
-
-// Counting Sort 
-// TC - O(N + K)
-// SC - O(K)
+// Can also be solved with sorting and hashing
 
 // Cyclic Sort 
 // TC - O(N)
 // SC - O(1)
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        int i=0;
+        while(i < nums.size()){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums[i] , nums[correct]);
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] != i+1){
+                ans.push_back(i+1);
+            }
+        }
+
+        return ans;
+    }
 
 // Inplace Negation 
 // TC - O(N)
 // SC - O(1)
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        
+        for(int i=0; i<nums.size(); ++i){
+            int correct = abs(nums[i]) - 1;
+            if(nums[correct] > 0) nums[correct] = -nums[correct];
+        }
 
-// Hashing 
-// TC - O(N) , worst O(N^2)
-// SC - O(N)
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] > 0){
+                ans.push_back(i+1);
+            }
+        }
+
+        return ans;
+    }
